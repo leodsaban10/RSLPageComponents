58fa52e7671afed4f7498fa8b2679e8afe27de74
leo.saban <leodsaban@outlook.com>
2025-04-05 20:00:15 -0600

    Created the two files i will be using to look for captcha

---
+++ test/pageobjects/homepage.js
@@ -0,0 +1,26 @@
+import { $, expect, browser } from '@wdio/globals'
+
+
+class HomePage extends Page {
+
+    get inputUsername () {
+        return $('#username');
+    }
+
+    get inputPassword () {
+        return $('#password');
+    }
+
+    get btnSubmit () {
+        return $('button[type="submit"]');
+    }
+
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+}
+
+export default new HomePage();
---
+++ test/specs/captchacheck.js
@@ -0,0 +1,13 @@
+import HomePage from '../pageobjects/homepage.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await HomePage.open()
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveText(
+            expect.stringContaining('You logged into a secure area!'))
+    })
+})
+
