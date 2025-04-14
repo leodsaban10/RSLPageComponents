1a10dc2517b3e9238db7001edb468b5635d8a6b9
leo.saban <leodsaban@outlook.com>
2025-04-14 07:40:56 -0600

    added two new functions for Latest news one and two along with their test functions

--- test/specs/captchacheck.js
+++ test/specs/captchacheck.js
@@ -1,33 +1,32 @@
 import HomePage from '../pageobjects/homepage.js'
 
 describe('Testing for Captcha', () => {
-    it('should open the RSL Home Page', async () => {
-        await HomePage.OpenRSLPage()
-    })
-    it('Should open the Home Page and the LeaderBoard Banner', async () => {
-        await HomePage.OpenRSLPage()
-        await HomePage.LeaderBoardBanner()
-    })
-    it('should open the Home Page and click the Adidas Logo', async () => {
-        await HomePage.OpenRSLPage()
-        await HomePage.AdidasLogo()
-    })
-    it ('should open rsl in spanish article one', async () => {
-        await HomePage.OpenRSLPage()
-        await HomePage.RSLEnEspanol_One()
-    })
-    it ('should open rsl in spanish article two', async () => {
-        await HomePage.OpenRSLPage()
-        await HomePage.RSLEnEspanol_Two()
-    })
-    // it('should open the Home Page and click the Social Media Icon', async () => {
-    //     await HomePage.Open()
-    //     await HomePage.DottedMenu()
+    // it('should open the RSL Home Page', async () => {
+    //     await HomePage.OpenRSLPage()
     // })
-    // it('should click the Espanol Button', async () => {
-    //     await HomePage.Open()
-    //     await HomePage.EspanolButton()
+    // it('Should open the Home Page and the LeaderBoard Banner', async () => {
+    //     await HomePage.OpenRSLPage()
+    //     await HomePage.LeaderBoardBanner()
     // })
-
+    // it('should open the Home Page and click the Adidas Logo', async () => {
+    //     await HomePage.OpenRSLPage()
+    //     await HomePage.AdidasLogo()
+    // })
+    // it ('should open rsl in spanish article one', async () => {
+    //     await HomePage.OpenRSLPage()
+    //     await HomePage.RSLEnEspanol_One()
+    // })
+    // it ('should open rsl in spanish article two', async () => {
+    //     await HomePage.OpenRSLPage()
+    //     await HomePage.RSLEnEspanol_Two()
+    // })
+    // it ('should open Latest News article one', async () => {
+    //     await HomePage.OpenRSLPage()
+    //     await HomePage.LatestNewsArticle_One()
+    // })
+    it('should open Latest News article two', async () => {
+        await HomePage.OpenRSLPage()
+        await HomePage.LatestNewsArticle_Two()
+    })
 })
 
